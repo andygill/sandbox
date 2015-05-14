ffa6604a84ef5b2829e4bcddab9fd3ca3098c1ac
Andy Gill <andygill@ku.edu>
2015-05-13 23:13:30 -0500

    Now with 2 effects

--- hermit/Experiment.hs
+++ hermit/Experiment.hs
@@ -9,21 +9,40 @@ import Data.Vector ((!))
 import Control.Monad
 import Data.Aeson.Types
 import Data.Monoid
+import Data.Proxy
+import Control.Applicative
 
 class (ToJSON e, Show e) => Effector e where
    toEffectH :: (Monad m, m ~ IO) => e -> m ()
 
+class Rewriter r where
+   toRewriter :: r a -> a -> IO a
+
 data TypedEffectH :: * where
   EffectH                :: Effector  e   => e                   -> TypedEffectH
+  RewriteInt             :: Rewriter  rr  => rr Int              -> TypedEffectH
 {-
-  RewriteLCoreTC         :: Rewriter  rr  => rr LCoreTC          -> TypedEffectH
-  RewriteLCore           :: Rewriter  rr  => rr LCore            -> TypedEffectH
+    RewriteLCore           :: Rewriter  rr  => rr LCore            -> TypedEffectH
   TransformLCorePathBoxH :: Transformer t => t LCore LocalPathH -> TypedEffectH
 -}
+
+------------------------------------------------------------------      
 
 instance Show TypedEffectH where
   show (EffectH e) = show e
   
+instance ToJSON TypedEffectH where
+  toJSON (EffectH e) = tagged "TypedEffectH" "EffectH" [toJSON e]
+
+instance FromJSON TypedEffectH where
+  parseJSON (Array a) = case V.toList a of
+    ["TypedEffectH","EffectH",e] -> 
+      EffectH <$> (parseJSON e :: Parser Effects    ) <|>
+      EffectH <$> (parseJSON e :: Parser MoreEffects)
+    _ -> mzero
+
+------------------------------------------------------------------      
+
 data Effects :: * where
   Effect1 :: Effects
   deriving Show
@@ -32,27 +51,33 @@ instance Effector Effects where
    toEffectH Effect1 = print ("effect(1)" :: String)
    
 instance ToJSON Effects where
-  toJSON Effect1 = enum "Effect" "Effect1"
-
-instance ToJSON TypedEffectH where
-  toJSON (EffectH e) = tagged "TypedEffectH" "EffectH" [toJSON e]
+  toJSON Effect1 = enum "Effects" "Effect1"
 
 instance FromJSON Effects where
   parseJSON (Array a) = case V.toList a of
-    ["Effect","Effect1"] -> return Effect1                
-    _ -> mzero
-
-instance FromJSON TypedEffectH where
-  parseJSON (Array a) = case V.toList a of
-    ["TypedEffectH","EffectH",e] -> parseJSON_Effector e EffectH
+    ["Effects","Effect1"] -> return Effect1                
     _ -> mzero
     
-parseJSON_Effector :: Value -> (forall e . Effector e => e -> k) -> Parser k
-parseJSON_Effector (Array a) k = case V.toList a of
-        ["Effect","Effect1"] -> return $ k Effect1
-        _ -> mzero
 
 ------------------------------------
+
+data MoreEffects :: * where
+  Effect2 :: MoreEffects
+  deriving Show
+  
+instance Effector MoreEffects where
+   toEffectH Effect2 = print ("effect(2)" :: String)
+   
+instance ToJSON MoreEffects where
+  toJSON Effect2 = enum "MoreEffects" "Effect2"
+
+instance FromJSON MoreEffects where
+  parseJSON (Array a) = case V.toList a of
+    ["MoreEffects","Effect2"] -> return Effect2               
+    _ -> mzero
+
+------------------------------------------------------------------      
+    
 
 effect1 :: Effects
 effect1 = Effect1
@@ -65,4 +90,9 @@ enum ty tag = toJSON [ty,tag]
 tagged :: String -> String -> [Value] -> Value
 tagged ty tag rest = Array $ V.fromList $ [toJSON ty, toJSON tag] ++ rest
 
-        
+transmit :: TypedEffectH -> Result TypedEffectH
+transmit e = fromJSON (toJSON e)
+
+main = do
+        print $ transmit $ EffectH $ effect1
+
